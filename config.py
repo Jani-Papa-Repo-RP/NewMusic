ff2d3aab0950288bdbebe3ca6cb8b0a0fc5935ff
Jani-Papa-Repo-RP <janiofficialgamer@gmail.com>
2026-06-12 11:56:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheHamkerAlone/NewMusic",
+    "https://github.com/Jani-Papa-Repo-RP/NewMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ALONE")
 GIT_TOKEN = getenv(
